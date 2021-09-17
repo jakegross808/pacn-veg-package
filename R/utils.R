8d81519b8aa40d549702aea36bf151ac64f90213
Sarah E. Wright <s.wright.618@gmail.com>
2021-09-17 13:51:14 -0700

    First pass at reading data in Need to figure out how to handle medium-large data

--- R/utils.R
+++ R/utils.R
@@ -53,8 +53,8 @@ LoadPACNVeg <- function(ftpc_params = "pacn", eips_paths, data_path, data_source
     eips_paths <- normalizePath(eips_paths, mustWork = FALSE)
     cache_path <- normalizePath(paste0(rappdirs::user_cache_dir(appname = "pacnvegetation"), "/pacnveg_cache_data.rds"), mustWork = FALSE)
     cache_expiration_path <- normalizePath(paste0(rappdirs::user_cache_dir(appname = "pacnvegetation"), "/pacnveg_cache_expiration.rds"), mustWork = FALSE)
-    cache_exists <- file.exists(cache_path) & file.exists(cache_expiration_path)
-    cache_valid <- cache_exists & (is.null(readRDS(cache_expiration_path)) ||  # If cache expiration set to NULL, always read from cache
+    cache_exists <- file.exists(cache_path) && file.exists(cache_expiration_path)
+    cache_valid <- cache_exists && (is.null(readRDS(cache_expiration_path)) ||  # If cache expiration set to NULL, always read from cache
                                      readRDS(cache_expiration_path) > Sys.time())
 
     # If cache = TRUE and isn't expired, read data from there, otherwise load from databases
@@ -63,12 +63,12 @@ LoadPACNVeg <- function(ftpc_params = "pacn", eips_paths, data_path, data_source
       data <- readRDS(cache_path)
     } else {
       # Verify that Access db(s) exist
-      if (!(all(grepl(".*\\.mdb$", eips_paths)) & all(file.exists(eips_paths)))) {
+      if (!(all(grepl(".*\\.mdb$", eips_paths)) && all(file.exists(eips_paths)))) {
         stop("Path(s) to EIPS Access database(s) are invalid")
       }
 
       # If FTPC params provided as cav, read it and store in a list
-      if (length(ftpc_params) == 1 & grepl(".*\\.csv$", ftpc_params)) {
+      if (length(ftpc_params) == 1 && grepl(".*\\.csv$", ftpc_params)) {
         ftpc_params <- readr::read_csv(ftpc_params, col_types = readr::cols(.default = readr::col_character()))
         ftpc_params <- as.list(ftpc_params)
       }
@@ -81,6 +81,7 @@ LoadPACNVeg <- function(ftpc_params = "pacn", eips_paths, data_path, data_source
       }
 
       ftpc_data <- ReadFTPC(ftpc_conn)
+      DBI::dbDisconnect(ftpc_conn)
       eips_data <- ReadEIPS(eips_paths)
 
       data <- c(ftpc_data, eips_data)
@@ -122,6 +123,7 @@ LoadPACNVeg <- function(ftpc_params = "pacn", eips_paths, data_path, data_source
   tbl_names <- names(data)
   lapply(tbl_names, function(n) {assign(n, data[[n]], envir = pkg_globals)})
 
+  invisible(data)
 }
 
 #' Read data from FTPC database
@@ -131,7 +133,86 @@ LoadPACNVeg <- function(ftpc_params = "pacn", eips_paths, data_path, data_source
 #' @return A list of tibbles
 #'
 ReadFTPC <- function(conn) {
-  data <- list()
+
+  # Tree density information collected for small trees (subalpine),
+  # large trees (forest, coastal, subalpine), and large tree ferns (forest).
+  LgWoodyIndividual <- dplyr::tbl(conn, "tbl_Lg_Woody_Individual") %>%
+    dplyr::select(Large_Woody_ID, Event_ID, Species_ID, Life_Form, Quad, Status, Height,
+           Height_Dead, Boles, DBH, DBH_Basal, Vigor, Fruit_Flower, Rooting, Foliar,
+           Caudex_Length, Shrublike_Growth, Resprouts, Measurement_Type) %>%
+    dplyr::collect()
+  # Subtable - Bole DBH for trees that have multiple boles.
+  LgWoodyMultipleBoles <- dplyr::tbl(conn, "tbl_Multiple_Boles") %>%
+    dplyr::select(Large_Woody_ID, DBH_Bole = DBH, Root_Sprout, Status_Bole = Status) %>%
+    dplyr::collect()
+  # Subtable - Extra data for dead trees
+  LgWoodySnags <- dplyr::tbl(conn, "tbl_Snags")%>%
+    dplyr::select(Large_Woody_ID, Basal_diameter, Height_Snag = Height) %>%
+    dplyr::collect()
+  # Subtable - Sprout size data collected in recently disturbed sites for individuals
+  # that have been top killed and are alive only by basal sprouts.
+  LgWoodyBasalSprout <- dplyr::tbl(conn, "tbl_Basal_Sprout") %>%
+    dplyr::select(Large_Woody_ID, Sprout_Ht, W1, W2, Area, Volume) %>%
+    dplyr::collect()
+
+  # Recorded within each plot by measuring the height of ~five canopy trees per
+  # plot that represent the average canopy height within the plot.
+  TreeCanopyHeight <- dplyr::tbl(conn, "tbl_Tree_Canopy_Height") %>%
+    dplyr::select(Event_ID, Species_ID, Quad, Status, Top, Base, Base_ht,
+           Distance, Height, Method, DBH, Comments) %>%
+    dplyr::collect()
+
+  # List of species present within plot.
+  Presence <- dplyr::tbl(conn, "tbl_Presence") %>%
+    dplyr::select(Event_ID, Species_ID, Fruit_Flower, Dead, Outside_Plot, cf, Comments) %>%
+    dplyr::collect()
+
+  # Count data, foliar height, and rooting height data tallied for
+  # vines, seedlings, shrubs, small trees, and small tree ferns.
+  SmWoodyTally <- dplyr::tbl(conn, "tbl_Sm_Woody_Tally") %>%
+    dplyr::select(Event_ID, Species_ID, Transect, Life_Form, DBH, Status,
+           Foliar, Rooting, Count, Comments) %>%
+    dplyr::collect()
+
+  # point-intercept cover of  species and substrate
+  # two stratum: 1)low [0-1m] 2) high [1-2m]
+  UnderstoryCover <- dplyr::tbl(conn, "tbl_Understory_Cover") %>%
+    dplyr::select(Event_ID, Point_ID, Point, Substrate)
+  UnderstoryLow <- dplyr::tbl(conn, "tbl_Understory_Cover") %>%
+    dplyr::select(Event_ID, Point_ID, Point, Substrate)
+  UnderstoryHigh <- dplyr::tbl(conn, "xref_Understory_High") %>%
+    dplyr::select(Event_ID, Point_ID, Species_ID, Dead)
+
+  UnderstoryLow <- dplyr::inner_join(UnderstoryCover, UnderstoryLow, by = "Point_ID") %>%
+    dplyr::mutate(Stratum = "Low")
+  UnderstoryHigh <- dplyr::inner_join(UnderstoryCover, UnderstoryHigh, by = "Point_ID") %>%
+    dplyr::mutate(Stratum = "High")
+
+  UnderstoryCover <- rbind(UnderstoryLow, UnderstoryHigh)
+
+  # Dead, downed wood and tree fern logs
+  # with a diameter >=7.6 at the point of planar intersection.
+  WoodyDebris <- dplyr::tbl(conn, "tbl_Woody_Debris") %>%
+    dplyr::select(Woody_Debris_ID, Event_ID, Transect, Length)
+  # Subtable - Additional information on debris type (wood/tree fern), diameter,
+  # and decay class
+  WoodyDebrisSpecies <- dplyr::tbl(conn, "tbl_Debris_Species") %>%
+    dplyr::select(Woody_Debris_ID, Debris_type, Diameter, Decay_Class, Comments)
+
+  WoodyDebris <- dplyr::left_join(WoodyDebris, WoodyDebrisSpecies, by = "Woody_Debris_ID") %>%
+    dplyr::collect()
+
+  data <- list(
+    LgWoodyIndividual = LgWoodyIndividual,
+    LgWoodyMultipleBoles = LgWoodyMultipleBoles,
+    LgWoodyBasalSprout = LgWoodyBasalSprout,
+    LgWoodySnags = LgWoodySnags,
+    TreeCanopyHeight = TreeCanopyHeight,
+    Presence = Presence,
+    SmWoodyTally = SmWoodyTally,
+    # UnderstoryCover = UnderstoryCover,
+    WoodyDebris = WoodyDebris
+  )
 
   return(data)
 }
