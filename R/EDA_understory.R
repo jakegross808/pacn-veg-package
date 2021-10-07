501844804a3555790e1cd9f816f8bef857278025
jakegross808 <jacob_gross@nps.gov>
2021-10-06 14:16:31 -1000

    UnderNativityCoverTotal updates

--- R/EDA_understory.R
+++ R/EDA_understory.R
@@ -13,20 +13,21 @@
 #' }
 UnderCombineStrata <- function(data) {
   if(!"Point" %in% colnames(data)){
-    stop("Invalid data table. Point data is missing")}
-  else {
+    stop("Invalid data table. Point data is missing")
+  }
+
   data_strat_comb <- data %>%
     #Stratum removed from grouping variable
-    select(-Stratum) %>%
+    dplyr::select(-Stratum) %>%
     unique() %>%
-    mutate(Stratum = "High and Low")
+    dplyr::mutate(Stratum = "High and Low")
   return(data_strat_comb)
-}}
+}
 
 
 #' Calculate Total Native & Nonnative Cover in Understory
 #'
-#' @param data An Understory tibble/dataframe with Nativity Point Data
+#' @inheritParams FilterPACNVeg
 #'
 #' @return Summary table of total Percent Cover by Nativity
 #' @export
@@ -36,33 +37,25 @@ UnderCombineStrata <- function(data) {
 #' data <- FilterPACNVeg("Understory")
 #' Native_Cover_Summary_table <- UnderNativityCoverTotal(data)
 #' }
-UnderNativityCoverTotal <- function(park, sample_frame, community, year, cycle, plot_type) {
+UnderNativityCoverTotal <- function(combine_strata = FALSE, park, sample_frame, community, year, cycle, plot_type, silent = FALSE) {
 
-  raw_data <- FilterPACNVeg("Understory", park, sample_frame, community, year, cycle, plot_type)
+  raw_data <- FilterPACNVeg("Understory", park, sample_frame, community, year, cycle, plot_type, is_qa_plot = FALSE, silent = silent)
+  if (combine_strata == TRUE) {
+    raw_data <- UnderCombineStrata(raw_data)
+  }
+
   # Calculate Total Native & Nonnative Cover by stratum
   Nat_Cov <- raw_data %>%
-    group_by(Unit_Code, Community, Sampling_Frame, Year, Cycle, Plot_Number, Point, Stratum, Nativity) %>%
-    summarise(Hits_All_Nat = n(), .groups = 'drop')  %>%
+    dplyr::group_by(Unit_Code, Community, Sampling_Frame, Year, Cycle, Plot_Number, Point, Stratum, Nativity) %>%
+    dplyr::summarise(Hits_All_Nat = dplyr::n(), .groups = 'drop')  %>%
     # Don't count record if point had no hits: (e.g. Point is NA )
-    mutate(Hits_All_Nat = replace(Hits_All_Nat, is.na(Nativity), 0)) %>%
+    dplyr::mutate(Hits_All_Nat = replace(Hits_All_Nat, is.na(Nativity), 0)) %>%
     # group by plot (i.e. remove Point from grouping variable)
-    group_by(Cycle, Unit_Code, Sampling_Frame, Plot_Number, Stratum, Nativity) %>%
+    dplyr::group_by(Cycle, Unit_Code, Sampling_Frame, Plot_Number, Stratum, Nativity) %>%
     #Total hits at each point for each strata for entire plot
     # (can be > 300 points or >100% because more than one native species can be present per point)
-    summarise(tot_pct_cov = (sum(Hits_All_Nat)) / 300 * 100, .groups = 'drop')
+    dplyr::summarise(tot_pct_cov = (sum(Hits_All_Nat)) / 300 * 100, .groups = 'drop')
 
   return(Nat_Cov)
 }
 
-# library(tidyverse)
-# undstr <- veg$Understory
-# fun1 <- undstr %>%
-#   group_by(Unit_Code, Community, Sampling_Frame, Year, Cycle, Plot_Number, Point, Stratum, Nativity) %>%
-#   summarize(Hits_All_Nat = n(), .groups = 'drop')  %>%
-#   # Don't count record if point had no hits: (e.g. Point is NA )
-#   mutate(Hits_All_Nat = replace(Hits_All_Nat, is.na(Nativity), 0)) %>%
-#   # group by plot (i.e. remove Point from grouping variable)
-#   group_by(Cycle, Unit_Code, Sampling_Frame, Plot_Number, Stratum, Nativity) %>%
-#   #Total hits at each point for each strata for entire plot
-#   # (can be > 300 points or >100% because more than one native species can be present per point)
-#   summarise(tot_pct_cov = (sum(Hits_All_Nat)) / 300 * 100, .groups = 'drop')
