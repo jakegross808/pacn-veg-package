ea764f418982d21cea3c9bdf239398e7708ccc94
jakegross808 <jacob_gross@nps.gov>
2021-11-08 15:12:32 -1000

    merge check

--- R/EDA_understory.R
+++ R/EDA_understory.R
@@ -37,16 +37,17 @@ UnderCombineStrata <- function(data) {
 #' data <- FilterPACNVeg("Understory")
 #' Native_Cover_Summary_table <- UnderNativityCoverTotal(data)
 #' }
-UnderNativityCoverTotal <- function(combine_strata = FALSE, paired_change = FALSE, park, sample_frame, community, year, cycle, plot_type, silent = FALSE) {
+UnderNativityCover <- function(combine_strata = FALSE, paired_change = FALSE, park, sample_frame, community, year, cycle, plot_type, silent = FALSE) {
 
   raw_data <- FilterPACNVeg("Understory", park, sample_frame, community, year, cycle, plot_type, is_qa_plot = FALSE, silent = silent)
+
   if (combine_strata == TRUE) {
     raw_data <- UnderCombineStrata(raw_data)
   }
 
   # Calculate Total Native & Nonnative Cover by stratum
   Nat_Cov <- raw_data %>%
-    #'*Dead Still present in Understory Data*
+    ## Dead Still present in Understory Data!!  - change default filter settings? Or remove entirely from database
     dplyr::filter(Dead==FALSE)  %>%
     ## Drop point records if point had no hits: (drop if 'Code == NA')
     tidyr::drop_na(Code)  %>%
@@ -86,7 +87,7 @@ UnderNativityCoverTotal <- function(combine_strata = FALSE, paired_change = FALS
 
 #' Native V. Non-native Cover Plot for Understory data
 #'
-#' @param data An Understory tibble/dataframe with Native and Non-native Cover
+#' @inheritParams UnderNativityCover
 #'
 #' @return Nat-v-Non Plot comparing Native and Non-native Cover (or Change in Cover)
 #' @export
@@ -94,78 +95,114 @@ UnderNativityCoverTotal <- function(combine_strata = FALSE, paired_change = FALS
 #' @examples
 #' \dontrun{
 #'
-#' Native_v_Nonnative_Plot <- UnderNativityCoverTotal.plot.(UnderNativityCoverTotal())
+#' Native_v_Nonnative_Plot <- UnderNativityCover.plot.nat_v_non(sample_frame = "Haleakala", sample_cycle = 2, paired_change = TRUE)
 #' }
 
-UnderNatCovTotal <- function(data, sample_cycle) {
+UnderNativityCover.plot.nat_v_non <- function(sample_cycle, cover.stat, combine_strata = FALSE, paired_change = FALSE, park, sample_frame, community, year, cycle, plot_type, silent = FALSE) {
 
+  data <- UnderNativityCover(combine_strata = combine_strata, paired_change = paired_change,
+                             park = park, sample_frame = sample_frame, community = community, year = year, cycle = cycle,
+                             plot_type = plot_type, silent = silent)
+
+  # Count records with Nativity == Unknown
   unks <- data %>%
     dplyr::filter(Nativity=="Unknown")
   unks <- sum(unks$tot_pct_cov, na.rm = T)
+  # Remove unknowns
+  data <- data %>%
+    dplyr::filter(Nativity != "Unknown")
+  # Statement of unknowns removed
+  message(paste0(unks, "% of [Unknown] nativity cover removed."))
+
+  # Remove alternate cover.stat so data can pivot
+  cover.stat.options <- c("tot_pct_cov", "chg_per_cycle")
+  remove.cover.stat <- cover.stat.options[!cover.stat.options %in% cover.stat]
 
   toplot <- data %>%
-    dplyr::filter(Nativity != "Unknown") %>%
-    # If plotting change remove:
-    dplyr::select(-tot_pct_cov) %>%
     dplyr::filter(Cycle == sample_cycle) %>%
-    # pivot
-    tidyr::pivot_wider(names_from = Nativity, values_from = chg_per_cycle)
-
-  message(paste0(unks, "% of [Unknown] nativity cover removed."))
+    dplyr::select(-remove.cover.stat) %>%
+    tidyr::pivot_wider(names_from = Nativity, values_from = cover.stat)
 
   #Get max value for plotting data
   toplot.max <- toplot %>%
     dplyr::ungroup() %>%
     dplyr::select(Native, `Non-Native`)
-  toplot.max <- max(c(abs(max(toplot.max)), abs(min(toplot.max))))
+  toplot.max <- max(c(abs(max(toplot.max, na.rm = TRUE)), abs(min(toplot.max, na.rm = TRUE))))
 
-  ########
-  ids <- factor(c("1.1", "2.1", "1.2", "2.2", "1.3"))
+  if (cover.stat == "tot_pct_cov") {
+    d <- expand.grid(x=0:(toplot.max + 5), y=0:(toplot.max + 5))
 
-  values <- data.frame(
-    id = ids,
-    value = c("1 Native (-)\nNon-native (-)\n",
-              "2 Native (-)\nNon-native (+)\n",
-              "3 Native (+)\nNon-native (++)\n",
-              "4 Native (++)\nNon-native (+)\n",
-              "5 Native (++)\nNon-native (-)\n"))
+    plot.nat_v_non <- ggplot2::ggplot(d, ggplot2::aes(x,y)) +
+      ggplot2::geom_tile(ggplot2::aes(fill = atan(y/x), alpha=(0.15))) +
+      ggplot2::scale_fill_gradient(high="red", low="green") +
+      ggplot2::theme(legend.position="none") +
+      ggplot2::geom_point(data = toplot,
+                          mapping = ggplot2::aes(x = Native, y = `Non-Native`),
+                          color = "black",
+                          size = 2) +
+      ggrepel::geom_text_repel(data = toplot,
+                               mapping = ggplot2::aes(x = Native, y = `Non-Native`, label = Plot_Number),
+                               min.segment.length = 0, seed = 42, box.padding = 0.5) +
+      ggplot2::ylab("Total Non-Native Cover") +
+      ggplot2::xlab("Total Native Cover") +
+      ggplot2::scale_x_continuous(breaks = scales::breaks_pretty(n = 10)) +
+      ggplot2::scale_y_continuous(breaks = scales::breaks_pretty(n = 10)) +
+      ggplot2::coord_cartesian(xlim = c(0,toplot.max), ylim = c(0,toplot.max)) +
+      ggplot2::facet_wrap(~Stratum + Sampling_Frame)
 
-  #Create a custom color scale
-  quad_c <- c("#cccccc","#d11141","#f37735","#C8E52A","#00b159")
+    return(plot.nat_v_non)
+  }
 
-  positions <- data.frame(
-    id = rep(ids, each = 4),
-    x = c(-100, 0, 0, -100,
-          -100, -100, 0, 0,
-          #-100, 0, 0, 0,
-          0, 0, 0, 100,
-          0, 0, 100, 100,
-          0, 100, 100, 0),
-    y = c(-100, -100, 0, 0,
-          0, 100, 100, 0,
-          #-100, 0, 0, -100,
-          0, 0, 100, 100,
-          0, 0, 0, 100,
-          -100, -100, 0, 0))
-  datapoly <- merge(values, positions, by = c("id"))
+  if (cover.stat == "chg_per_cycle") {
+    ids <- factor(c("1.1", "2.1", "1.2", "2.2", "1.3"))
 
-  ggplot2::ggplot(data = datapoly,
-                  mapping = ggplot2::aes(x = x, y = y)) +
-    ggplot2::geom_polygon(ggplot2::aes(fill = value)) + # cannot do alpha w/coord_cartesian???
-    ggplot2::scale_fill_manual(values = quad_c) +
-    ggplot2::geom_point(data = toplot,
-                        mapping = ggplot2::aes(x = Native, y = `Non-Native`),
-                        color = "black",
-                        size = 2) +
-    ggrepel::geom_text_repel(data = toplot,
-                             mapping = ggplot2::aes(x = Native, y = `Non-Native`, label = Plot_Number),
-                             min.segment.length = 0, seed = 42, box.padding = 0.5) +
-    ggplot2::ylab("Change in Non-Native Cover") +
-    ggplot2::xlab("Change in Native Cover") +
-    ggplot2::geom_vline(xintercept = 0) +
-    ggplot2::geom_hline(yintercept = 0) +
-    ggplot2::scale_x_continuous(breaks = scales::breaks_pretty(n = 10)) +
-    ggplot2::scale_y_continuous(breaks = scales::breaks_pretty(n = 10)) +
-    ggplot2::coord_cartesian(xlim = c(-toplot.max, toplot.max), ylim = c(-toplot.max, toplot.max)) +
-    ggplot2::facet_wrap(~Stratum + Sampling_Frame)
+    values <- data.frame(
+      id = ids,
+      value = c("1 Native (-)\nNon-native (-)\n",
+                "2 Native (-)\nNon-native (+)\n",
+                "3 Native (+)\nNon-native (++)\n",
+                "4 Native (++)\nNon-native (+)\n",
+                "5 Native (++)\nNon-native (-)\n"))
+
+    #Create a custom color scale
+    quad_c <- c("#cccccc","#d11141","#f37735","#C8E52A","#00b159")
+
+    positions <- data.frame(
+      id = rep(ids, each = 4),
+      x = c(-100, 0, 0, -100,
+            -100, -100, 0, 0,
+            #-100, 0, 0, 0,
+            0, 0, 0, 100,
+            0, 0, 100, 100,
+            0, 100, 100, 0),
+      y = c(-100, -100, 0, 0,
+            0, 100, 100, 0,
+            #-100, 0, 0, -100,
+            0, 0, 100, 100,
+            0, 0, 0, 100,
+            -100, -100, 0, 0))
+    datapoly <- merge(values, positions, by = c("id"))
+
+    plot.nat_v_non <- ggplot2::ggplot(data = datapoly,
+                                      mapping = ggplot2::aes(x = x, y = y)) +
+      ggplot2::geom_polygon(ggplot2::aes(fill = value)) + # cannot do alpha w/coord_cartesian???
+      ggplot2::scale_fill_manual(values = quad_c) +
+      ggplot2::geom_point(data = toplot,
+                          mapping = ggplot2::aes(x = Native, y = `Non-Native`),
+                          color = "black",
+                          size = 2) +
+      ggrepel::geom_text_repel(data = toplot,
+                               mapping = ggplot2::aes(x = Native, y = `Non-Native`, label = Plot_Number),
+                               min.segment.length = 0, seed = 42, box.padding = 0.5) +
+      ggplot2::ylab("Change in Non-Native Cover") +
+      ggplot2::xlab("Change in Native Cover") +
+      ggplot2::geom_vline(xintercept = 0) +
+      ggplot2::geom_hline(yintercept = 0) +
+      ggplot2::scale_x_continuous(breaks = scales::breaks_pretty(n = 10)) +
+      ggplot2::scale_y_continuous(breaks = scales::breaks_pretty(n = 10)) +
+      ggplot2::coord_cartesian(xlim = c(-toplot.max, toplot.max), ylim = c(-toplot.max, toplot.max)) +
+      ggplot2::facet_wrap(~Stratum + Sampling_Frame)
+
+    return(plot.nat_v_non)
+  }
   }
