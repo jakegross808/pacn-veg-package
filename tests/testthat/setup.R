5e07a0ace4d254cd7f38d22fd1bc53d1c8fea1a8
jakegross808 <jacob_gross@nps.gov>
2022-09-09 11:29:59 -1000

    commit needed before merge

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,8 +1,8 @@
 # Load data from db, skip this test if no db connection
 tryCatch(db <- LoadPACNVeg(ftpc_params = "pacnveg",
-                           eips_paths = here::here("tests", "testthat", "dbs", c("2021_established_invasives_1_20210129.mdb",
-                                                         "2021_established_invasives_2_20210129.mdb",
-                                                         "established_invasives_BE_master_20210818.mdb")),
+                           eips_paths = here::here("tests", "testthat", "dbs", c("2021_established_invasives_20220902.mdb",
+                                                         "2022_established_invasives_20220907.mdb",
+                                                         "established_invasives_BE_master_20220503.mdb")),
                            cache = FALSE
 ),
 error = function(e) {
