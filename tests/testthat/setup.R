bbe9ac3e1be8606f3017b7987bea0995e1132c13
Wright <sewright@nps.gov>
2022-09-12 12:24:01 -0700

    Fix indentation

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,12 +1,11 @@
 # Load data from db, skip this test if no db connection
 tryCatch(db <- LoadPACNVeg(ftpc_params = "pacnveg",
                            eips_paths = here::here("tests", "testthat", "dbs", c("2021_established_invasives_20220902.mdb",
-                                                         "2022_established_invasives_20220907.mdb",
-                                                         "established_invasives_BE_master_20220503.mdb")),
-                           cache = FALSE
-),
-error = function(e) {
-  if (grepl(".*Could not open a connection to SQL Server.*", e$message)) {
-    skip("No database connection")
-  } else {stop(e)}
-})
+                                                                                 "2022_established_invasives_20220907.mdb",
+                                                                                 "established_invasives_BE_master_20220503.mdb")),
+                           cache = FALSE),
+         error = function(e) {
+           if (grepl(".*Could not open a connection to SQL Server.*", e$message)) {
+             skip("No database connection")
+           } else {stop(e)}
+         })
